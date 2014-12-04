fa2580bcd496d7d05e759f7c1154714fce2d734d
Peter+Julian <peter+julian@appacademy.io>
2014-12-04 09:02:12 -0500

    Initialize.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ninety9Cats::Application.config.secret_key_base = '777098915e3659f620cdee79924699aac2682b568e0d1468cb3bbd5e85bd8b7d8ea2772c8213c09fee4fab561d8633d07e3caf8e25a7ec995fedaa120a2226b3'
